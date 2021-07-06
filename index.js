8feb656c1caae8132c1f00bf029104a379d69e51
Ashmn07 <ashwin.menon202@gmail.com>
2021-07-06 21:01:07 +0530

    Preparing for Production

--- index.js
+++ index.js
@@ -20,6 +20,14 @@ mongoose.connection.on("error",()=>{
     console.log("err connecting",err);
 })
 
+if(process.env.NODE_ENV=="production"){
+    app.use(express.static('client/build'))
+    const path = require('path')
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
+
 app.listen(PORT,()=>{
     console.log("Server is running",PORT);
 })
